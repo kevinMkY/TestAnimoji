0426b370d88ebc088f052246b6aac667b1259e55
yekaihua <yekaihua@oneniceapp.com>
2017-11-20 20:24:44 +0800

    Initial Commit

---
+++ TestAnimoji/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAnimoji
+//
+//  Created by kevin on 2017/11/20.
+//  Copyright © 2017年 kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestAnimoji/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestAnimoji
+//
+//  Created by kevin on 2017/11/20.
+//  Copyright © 2017年 kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
